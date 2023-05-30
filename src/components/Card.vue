59da7ba2f3ba4e1f7c2c87bf8136e431ccfd8f24
Dmitriy <johndoe@example.com>
2023-05-30 12:01:05 +0300

    based created card and cardlist

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="card">
+        <div v-if="card">
+            <div>{{ card.title }}</div>
+            <img :src="'src/assets/images/' + card.img" alt="">
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'CardComp',
+    props: {
+        card: {
+            type: Object,
+            default: null
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.card {
+    border-radius: 8px;
+}
+</style>
