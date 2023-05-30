95f6efa3473a6e005db0335fca47e610f765e333
Dmitriy <johndoe@example.com>
2023-05-30 08:58:41 +0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+</template>
+
+<style >
+
+</style>
