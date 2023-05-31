7616bf4069c80eaade9e1d71348b42d19e8afb9e
Dmitriy <johndoe@example.com>
2023-05-31 13:06:20 +0300

    created delivery btn

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MainComp from "./components/MainComp.vue";
 import CardsList from "./components/CardsList.vue";
+import DeliveryBtn from "./components/DeliveryBtn.vue";
 </script>
 
 <template>
   <MainComp></MainComp>
   <CardsList></CardsList>
+  <DeliveryBtn></DeliveryBtn>
 </template>
 
 <style >
---
+++ src/components/DeliveryBtn.vue
@@ -0,0 +1,47 @@
+<template>
+    <button class="delivery-btn">
+        Замовити доставку
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'DeliveryBtn',
+}
+</script>
+
+<style lang="scss">
+.delivery-btn {
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    aspect-ratio: 1;
+    width: 195px;
+    font-family: 'TTTravels-DemiBold';
+    line-height: 130%;
+    letter-spacing: 0.03rem;
+    background: #5A30F0;
+    color: white;
+    text-transform: uppercase;
+    padding: 50px 25px;
+    border-radius: 50%;
+    border: none;
+    outline: none;
+    animation-name: rotateBtn;
+    animation-duration: 10s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    cursor: pointer;
+}
+@keyframes rotateBtn {
+    0% {
+        transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    50% {
+        transform: translate(-50%, -50%) rotate(45deg);
+    }
+    100% {
+        transform: translate(-50%, -50%) rotate(-45deg);
+    }
+}
+</style>
